e11746da96ff9a1c729e161416835aa3b647be89
Andreas Hedin <ah223vp@free-lse16.cartesia.local>
2017-10-02 15:50:46 +0200

    Comments added, diagrams added, .jar file working

---
+++ ws2/SourceCode/src/com/registry/Main.java
@@ -0,0 +1,18 @@
+package com.registry;
+
+
+import com.registry.model.DBControl;
+import com.registry.model.IDBControl;
+import com.registry.view.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        IDBControl xC = new DBControl();
+        Console vC = new Console(xC);
+        vC.getInput();
+
+    }
+}
---
+++ ws2/SourceCode/src/com/registry/model/IDBControl.java
@@ -0,0 +1,48 @@
+package com.registry.model;
+
+
+import java.util.List;
+
+public interface IDBControl {
+
+    /**
+     * Retrieves the member list object for handling
+     * @return List with member objects
+     */
+    List<Member>getMembers();
+
+    /**
+     * Save method. Tells the "DB" to save the current DBFile
+     */
+    void saveMember();
+
+    /**
+     * Adding a member to dbList and then writing to the XML file
+     * @param member to be added in the XML file and list
+     */
+    void addMember(Member member);
+
+    /**
+     * Returning a specific member object to the caller
+     * @param id Member id to search the db upon
+     * @return Member object to manipulate
+     */
+    Member getMember(int id);
+
+    /**
+     * Check to see if the member exists. Make error handling easier to have a "pre" check.
+     * @param id id to search for
+     * @return boolean depending on the user exists or not
+     */
+    boolean memberExists(int id);
+
+    /**
+     * Deleting a seleceted member from list and file
+     * @param member to be deleted from list and file
+     */
+    void deleteMember(Member member);
+
+
+
+
+}
